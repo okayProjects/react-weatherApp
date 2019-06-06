fa99b9cb2757adbe87f54e9d3d57cf071554c09e
unknown <lugowski.k@gmail.com>
2019-06-06 15:17:46 +0200

    Updates

---
+++ precache-manifest.15a5701931757adba56640255b57aa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f068a13fe86fff1d452012248d8d7c",
+    "url": "/react-weatherApp/./index.html"
+  },
+  {
+    "revision": "8e4e2c65afe928efd994",
+    "url": "/react-weatherApp/./static/css/main.5e230f8f.chunk.css"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/react-weatherApp/./static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "8e4e2c65afe928efd994",
+    "url": "/react-weatherApp/./static/js/main.73869631.chunk.js"
+  },
+  {
+    "revision": "2edf27dbf66d6f775b17",
+    "url": "/react-weatherApp/./static/js/runtime~main.45aff06b.js"
+  },
+  {
+    "revision": "2c39b06689b4ff91454932e4d14b59c0",
+    "url": "/react-weatherApp/./static/media/clouds.2c39b066.jpg"
+  }
+]);
